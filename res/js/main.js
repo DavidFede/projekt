794efd77b493c0b84653c0cb0b5e62ba80743216
DavidFede <david.sandarivskyj@student.spsmb.cz>
2023-09-19 15:35:57 +0200

    click upgrade

--- res/js/main.js
+++ res/js/main.js
@@ -1,10 +1,51 @@
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
+const clickUpgrade = document.getElementById("clickUpgrade");
+
 
 let numberOfCookies = 0;
+let constOfClickUpgrade = 20;
+let clickUpgradeIncrease = 1;
 
+// {} - scope
 cookie.onclick = () => {
-    numberOfCookies++;
+    numberOfCookies += clickUpgradeIncrease;
     //counter.innerHTML = "Cookies: " + numberOfCookies;
+    //numberOfCookies += 1;
+    // ++ inkrement
     counter.innerHTML = `Cookies: ${numberOfCookies}`;
+}
+
+const clickUpgradeFunction = () => {
+    if (numberOfCookies >= constOfClickUpgrade) {
+        //odecteme cenu upgrade
+        //pocet cookies a odectu 20
+        numberOfCookies -= constOfClickUpgrade
+        //aktualizace odstavce
+        counter.innerText = "Cookies; " + numberOfCookies;
+
+
+        //zvedneme pocet cookies na kliknuti
+        clickUpgradeIncrease++;
+    }
+};
+
+clickUpgrade.onclick = clickUpgradeFunction;
+
+
+
+//Vytvoreni funkce
+//function nazev() {}
+function cheats(){
+    console.log("Cheats on!")
+    numberOfCookies += 1000000
+    counter.innerHTML = `Cookies: ${numberOfCookies}`;
+}
+
+//Spusteni funkce
+cheats();
+
+cookie.cheatsTwo = () => {
+    numberOfCookies *= 2
+    counter.innerText = "Cookies" + numberOfCookies;
 }
