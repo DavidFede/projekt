f1c7e90f9df8a294547fd0627a43c9956b595067
DavidFede <david.sandarivskyj@student.spsmb.cz>
2023-09-26 14:52:17 +0200

    cookieautoclicker

--- res/js/main.js
+++ res/js/main.js
@@ -1,16 +1,44 @@
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
 const clickUpgrade = document.getElementById("clickUpgrade");
+const autoClicker = document.getElementById("autoClicker")
+//promenna const. let. var. let se da menit const se neda
+//
 
 
 let numberOfCookies = 0;
 let constOfClickUpgrade = 20;
 let clickUpgradeIncrease = 1;
+let autoclickIncrease = 0;
+let autoclicker;
+let autoClickerCost = 100;
 
-// {} - scope
+autoClicker.onclick= () => {
+    if (numberOfCookies >= autoClickerCost) {
+        //odecist cookies
+        numberOfCookies -= autoClickerCost;
+        autoClickerCost *= 2;
+        autoClicker.innerText = "Buy autoclicker upgrade" + autoClickerCost
+
+        //numberOfCookies = numberOfCookies -100;
+        //aktualizovat odstavec s cookies
+        counter.innerHTML = `Cookies: ${numberOfCookies}`;
+        autoclickIncrease ++;
+        //clear
+        clearInterval(autoclicker)
+        //spustit interval
+        autoclicker = setInterval(() => {
+            //za kazdou vterinu zvednout nejake cislo
+            numberOfCookies +=autoclickIncrease
+            //aktualizovat odstavec
+            counter.innerHTML = `Cookies: ${numberOfCookies}`;
+        }, 1000);
+    }
+}
+// {} - scope rozsah kam piseme priklady definice funkce
 cookie.onclick = () => {
     numberOfCookies += clickUpgradeIncrease;
-    //counter.innerHTML = "Cookies: " + numberOfCookies;
+    //counter.innerHTML = "Cookies: " + numberOfCookies; 
     //numberOfCookies += 1;
     // ++ inkrement
     counter.innerHTML = `Cookies: ${numberOfCookies}`;
@@ -44,8 +72,11 @@ function cheats(){
 
 //Spusteni funkce
 cheats();
+//cheatsTwo() spusteni funkce
 
-cookie.cheatsTwo = () => {
+const cheatsTwo = () => {
     numberOfCookies *= 2
     counter.innerText = "Cookies" + numberOfCookies;
 }
+cheatsTwo();
+
